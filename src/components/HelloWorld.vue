326929668baf00a20ded3908950f21811ba7d7e5
mupsy <amm@muxinsq.com>
2022-09-29 23:28:56 +0800

    vvc

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <div style="{color: color}">
+        <slot>Hello World!</slot>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+    export default {
+        props: {
+            color: String
+        }
+    }
+</script>
