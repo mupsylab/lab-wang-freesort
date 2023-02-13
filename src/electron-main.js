bb8df397720299f8d9b48f1e8fb6773553ea4d28
mupsycho <amm@mupsy.net>
2023-02-13 16:25:16 +0800

    you can now make it become a exe or dmg to run

---
+++ src/electron-main.js
@@ -0,0 +1,26 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;  // 用来保存主窗口对象的引用
+
+// 当 Electron 完成初始化并准备创建浏览器窗口时被调用
+app.on('ready', () => {
+  // 创建主窗口
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  // 加载页面文件
+  if (app.isPackaged) {
+    // 如果是打包好的就加载打包的 HTML 文件
+    mainWindow.loadFile('dist/index.html');
+  }else {
+    // 如果没有打包就直接从本地服务器加载
+    mainWindow.loadURL('http://localhost:8080/');
+  }
+
+  // 关闭事件
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
+  publicPath: './',
+  devServer: {
+    port: 8080
+  },
+  chainWebpack: config => {
+    config
+      .plugin("html")
+      .tap(args => {
+        args[0].title = "行人过街决策的研究";
+        return args;
+      })
+  }
 })
